80ea73ef630b0f467437d59019f562843de89164
MaercioMamedes <maerciomamedes@hotmail.com>
2022-09-07 09:23:15 -0300

    debug true

--- gameFinance/settings.py
+++ gameFinance/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fuvlz&e98-0w_13usa56+$l-+darpt#@sc+tiqv=$7fo=vdqdi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
